da7ba6350e986a6b84b63efb17ea665587f5cfe4
kazunobu <k-ichihashi@hokkai-v.co.jp>
2014-09-09 15:00:59 +0900

    新しいバージョンに対応

--- socke.io/05.namespace/app.js
+++ socke.io/05.namespace/app.js
@@ -1,24 +1,15 @@
-/*
-var app = require('express').createServer()
-  , io = require('socket.io').listen(app);
-
-io.set('log level', 2);
-app.listen(8080);
-*/
-var app = require("express")(),
-server = require("http").createServer(app),
-io = require("socket.io").listen(server);
+var app = require("express")();
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
 
 server.listen(8080);
-io.set('log level', 2);
 
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+app.get('/:file?', function (req, res) {
+  if (req.params.file === undefined) {
+    req.params.file = 'index.html';
+  }
+  res.sendfile(__dirname + '/' + req.params.file);
 });
-app.get('/jquery-1.7.2.min.js', function (req, res) {
-  res.sendfile(__dirname + '/jquery-1.7.2.min.js');
-});
-
 
 var chat = io
   .of('/chat') // chat ‚ÌƒCƒxƒ“ƒg‚É‚È‚é
