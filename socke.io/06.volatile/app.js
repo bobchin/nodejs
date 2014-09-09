eb0d65fe59ac92f5e7e572d86b51d1cb0df17c7f
kazunobu <k-ichihashi@hokkai-v.co.jp>
2014-09-09 17:41:22 +0900

    新しいバージョンに対応

--- socke.io/06.volatile/app.js
+++ socke.io/06.volatile/app.js
@@ -1,29 +1,21 @@
-/*
-var app = require('express').createServer()
-  , io = require('socket.io').listen(app);
-
-io.set('log level', 2);
-app.listen(8080);
-*/
-var app = require("express")(),
-server = require("http").createServer(app),
-io = require("socket.io").listen(server);
+var app = require("express")();
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
 
 server.listen(8080);
-io.set("log level", 2);
 
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
-});
-app.get('/jquery-1.7.2.min.js', function (req, res) {
-  res.sendfile(__dirname + '/jquery-1.7.2.min.js');
+app.get('/:file?', function (req, res) {
+  if (req.params.file === undefined) {
+    req.params.file = 'index.html';
+  }
+  res.sendfile(__dirname + '/' + req.params.file);
 });
 
 io.sockets.on('connection', function (socket) {
   var i = 0;
   var roop = setInterval(function() {
     socket.volatile.emit('volatile msg', ++i);
-  }, 1000);
+  }, 100);
 
   socket.on('stop', function(){
     clearInterval(roop);
