1871115e1ae518f7763b351db7891ab4cf6bf7ba
kazunobu <k-ichihashi@hokkai-v.co.jp>
2014-09-09 18:23:39 +0900

    新しいバージョンに対応

--- socke.io/08.broadcast/app.js
+++ socke.io/08.broadcast/app.js
@@ -1,28 +1,23 @@
-/*
-var app = require('express').createServer()
-  , io = require('socket.io').listen(app);
+var app = require('express')();
+var server = require("http").Server(app);
+var io = require('socket.io')(server);
 
-io.set('log level', 2);
-app.listen(80);
-*/
-var app = require('express')(),
-server = require("http").createServer(app),
-io = require('socket.io').listen(server);
-
-io.set('log level', 2);
 server.listen(8080);
 
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
-});
-app.get('/jquery-1.7.2.min.js', function (req, res) {
-  res.sendfile(__dirname + '/jquery-1.7.2.min.js');
+app.get('/:file?', function (req, res) {
+  if (req.params.file === undefined) {
+  	req.params.file = 'index.html';
+  }
+  res.sendfile(__dirname + '/' + req.params.file);
 });
 
 var i = 0;
 io.sockets.on('connection', function (socket) {
   i++;
+  // 自分以外に一斉に出される。
   socket.broadcast.emit('user connected', i);
+  socket.emit('user connected', i);
+  
   socket.emit('con', i);
   console.log(i + 'conneted');
 });
