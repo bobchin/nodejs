d7c167ca2fb797955c5ddd0d775c87c548c10940
kazunobu <k-ichihashi@hokkai-v.co.jp>
2014-09-11 09:21:53 +0900

    Broadcastを使ったパターンにしてみた

--- socke.io/GetStarted/index.js
+++ socke.io/GetStarted/index.js
@@ -15,7 +15,7 @@ io.on('connection', function(socket){
 	});
 
 	socket.on('chat message', function(msg){
-		io.emit('chat message', msg);
+		socket.broadcast.emit('chat message', msg);
 	});
 });
 
