e5c5de070be2e98c0cef14bfb79c11254b978cbc
kazunobu <k-ichihashi@hokkai-v.co.jp>
2015-08-21 15:40:09 +0900

    if-statement.jsを追加

---
+++ javascripting/if-statement.js
@@ -0,0 +1,7 @@
+var fruit = 'orange';
+
+if (5 < fruit.length) {
+  console.log('The fruit name has more than five characters.');
+} else {
+  console.log('The fruit name has five characters or less.');
+}
